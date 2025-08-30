aa73504ff5313b45c109cef258ce2de65024af9f
Xaitan <utrustad.sprang_0z@icloud.com>
2025-08-30 14:26:13 +0200

    Ch1-L5

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ import (
 func main() {
 	const port = "8080"
 
+	// Serve files from the current directory
+	fs := http.FileServer(http.Dir("."))
+
+	// Handle root URL by serving files
 	mux := http.NewServeMux()
+	mux.Handle("/", fs)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
